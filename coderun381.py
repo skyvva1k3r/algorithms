2afabb88f516c825d0404547638fc383059d1f52
skyLaptop <antargtig@inbox.ru>
2025-10-02 11:10:54 +0300

    Coderun381

--- coderun381.py
+++ coderun381.py
@@ -1,16 +1,20 @@
 import sys
 
-
 def main():
-    month = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday", "Saturday"]
+    month = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     n = input().split()
-    days, limit = int(n[0]), month.index(n[1])
-    print(days, limit)
-    a = 10
-    for i in range(days//7):
-        days -= 7
-        print()
-        for j in range(days%7):
-            print(".", end = " ")
+    days, weekday, temp = int(n[0]), month.index(n[1]), []
+    for i in range(((days+6+weekday)//7)):
+        temp = []
+        for j in range(7):
+            if i*7+j-weekday+1 > days: 
+                break
+            if i*7+j < weekday:
+                temp.append("..")
+            else:
+                temp.append(f"{i*7+j-weekday+1}".rjust(2, "."))
+        print(*temp)
+
+
 if __name__ == '__main__':
     main()
