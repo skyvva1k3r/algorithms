5fbff18e4e58c8e3f18895cee2484e87482ece08
skyPC <kakoutohuk@gmail.com>
2025-10-08 00:30:12 +0300

    coderun36.py

--- coderun36.py
+++ coderun36.py
@@ -1,36 +1,19 @@
-import sys
-from itertools import permutations
-
 def main():
     l, n = map(int, input().split())
-    result = 10**9
-    splits = input().split()
-    varis = []
-    thread = [0, l]
+    cuts = list(map(int, input().split()))
+    cuts = [0] + sorted(cuts) + [l]
 
-    for p in permutations(splits):
-        varis.append("".join(p))
-    
-    print(varis)
+    dp = [[0] * (n + 2) for _ in range(n + 2)]
 
-    for i in range(len(varis)):
-        temp = 0
-        for j in range(len(splits)):
-            for k in range(len(thread)):
-                if thread[k-1] < (l - int(varis[i][j])) and thread[k] > (l - int(varis[i][j])):
-                    print(thread[k])
-                    temp += thread[k]
-                    break
-            thread.append(int(varis[i][j]))
-            thread.sort()
-            print("\n\n\n", temp, thread, varis[i][j], "\n\n\n", sep = "\n")
-        print("--------------------------------------------------------")
-        thread = [0, l]
-        if result > temp:
-            result = temp
-    print(result) 
-  
+    for length in range(2, n + 2):
+        for i in range(n + 2 - length):
+            j = i + length
+            dp[i][j] = float('inf')
+            for k in range(i + 1, j):
+                cost = cuts[j] - cuts[i] + dp[i][k] + dp[k][j]
+                dp[i][j] = min(dp[i][j], cost)
 
+    print(dp[0][n + 1])
 
 if __name__ == '__main__':
     main()
